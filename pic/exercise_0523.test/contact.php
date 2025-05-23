aedcd762a845258438f2620773639959146f659a
Rihongan <atzs19960904@gmail.com>
2025-05-23 16:46:26 +0800

    0523

---
+++ pic/exercise_0523.test/contact.php
@@ -0,0 +1,21 @@
+<?php
+$username = $_POST["username"];
+?>
+<!DOCTYPE html>
+<html lang="zh-tw">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>表單結果</title>
+</head>
+
+<body form action=" " method="get">
+    <h1>$username 您好 !</h1>
+    <p>★歡迎來到我的購物商城★</p>
+
+
+
+</body>
+
+</html>
