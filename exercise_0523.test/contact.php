906ea7663c761e71fc893bc5f81f9fad2fce5815
Rihongan <atzs19960904@gmail.com>
2025-05-28 22:10:12 +0800

    first commit

---
+++ exercise_0523.test/contact.php
@@ -0,0 +1,23 @@
+<?php
+$username = $_POST["username"] ;
+echo  " $username , Hello!";
+?>
+
+<!DOCTYPE html>
+<html lang="zh-TW">
+
+<head>
+    <meta charset="UTF-8">
+    <title>歡迎來到 Rihong Shopping Store</title>
+</head>
+
+<body>
+    <center>
+        <img src="Rihong Shopping Store LOGO.png" width="200PX" height="200PX" alt="Rihong. Shopping Store LOGO" />
+        <p><i>★ Welcome to Rihong. Shopping Store ★</i></p>
+        <p><i> - HAVE A NICE DAY - </i></p>
+
+</body>
+</center>
+
+</html>
